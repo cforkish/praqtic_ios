75f41416c4652acb64b93db237cf30a9ad43bbb3
Charlie Forkish <cforkish@gmail.com>
2014-03-29 09:55:17 -0700

    Initial Commit

---
+++ praqtic_ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  praqtic_ios
+//
+//  Created by Charles Forkish on 3/29/14.
+//  Copyright (c) 2014 Praqtic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAAppDelegate class]));
+    }
+}
---
+++ praqtic_iosTests/praqtic_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  praqtic_iosTests.m
+//  praqtic_iosTests
+//
+//  Created by Charles Forkish on 3/29/14.
+//  Copyright (c) 2014 Praqtic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface praqtic_iosTests : XCTestCase
+
+@end
+
+@implementation praqtic_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
